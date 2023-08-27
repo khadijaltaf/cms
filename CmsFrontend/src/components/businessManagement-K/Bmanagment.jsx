084ece3e0e1a04ba03556bd816121cca03f22504
khadijaltaf <khadijaltaf008@gmail.com>
2023-08-28 02:01:29 +0500

    Login commit

--- CmsFrontend/src/components/businessManagement-K/Bmanagment.jsx
+++ CmsFrontend/src/components/businessManagement-K/Bmanagment.jsx
@@ -1,8 +1,88 @@
 import React from 'react'
+import './Bmanagment.css'
 
 const Bmanagment = () => {
   return (
-    <div>Bmanagment</div>
+    <div>
+      <div>
+        <br /><br />
+        <button className='btn btn-secondary aa'>Manage</button>
+        <button class="btn btn-primary aa" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight">Add Blog</button>
+<br /><br /><br />
+        <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
+          <div class="offcanvas-header">
+            <h5 class="offcanvas-title" id="offcanvasRightLabel">Create Blog</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+          </div>
+          <div class="offcanvas-body">
+            <form>
+              <div class="mb-3">
+                <label for="exampleInputText" class="form-label">Title</label>
+                <input type="text" class="form-control" />
+                <div id="emailHelp" class="form-text">Will appear as the title .</div>
+              </div>
+              <div class="mb-3">
+                <label for="exampleInputText" class="form-label">Blog</label>
+                <input type="text" class="form-control" />
+              </div>
+
+              <div class="input-group mb-3">
+                <input type="file" class="form-control" id="inputGroupFile02" />
+                <label class="input-group-text" for="inputGroupFile02">Upload</label>
+              </div>
+
+              <button type="submit" class="btn btn-primary">Publish</button>
+            </form>
+          </div>
+        </div>
+      </div>
+      <table className="table">
+        <thead>
+          <tr>
+            <th scope="col">Action</th>
+            <th scope="col">Sno.</th>
+            <th scope="col">Title</th>
+            <th scope="col">Author Name</th>
+
+            <th scope="col">Publishing Date</th>
+            <th scope="col">Updated Date</th>
+
+
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row"><button className=' btn btn-success'>open</button><button className=' btn btn-danger'>Delete</button><button className=' btn btn-primary'>Edit</button></th>
+            <th>01</th>
+            <td>Mark</td>
+            <td>Mark</td>
+
+            <td>Otto</td>
+            <td>@mdo</td>
+
+
+          </tr>
+          <tr>
+            <th scope="row"><button className=' btn btn-success'>open</button><button className=' btn btn-danger'>Delete</button><button className=' btn btn-primary'>Edit</button></th>
+            <th>02</th>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>Mark</td>
+
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <th scope="row"><button className=' btn btn-success'>open</button><button className=' btn btn-danger'>Delete</button><button className=' btn btn-primary'>Edit</button></th>
+            <th>03</th>
+            <td>Larry </td>
+            <td>Mark</td>
+            <td>@twitter</td>
+            <td>Mark</td>
+
+          </tr>
+        </tbody>
+      </table>
+    </div>
   )
 }
 
--- CmsFrontend/src/components/login-A/Login.jsx
+++ CmsFrontend/src/components/login-A/Login.jsx
@@ -1,9 +1,35 @@
 import React from 'react';
 import { Link, Outlet } from 'react-router-dom';
+import './Login.css'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div><main class="main">
+    <div class="container">
+      <section class="wrapper">
+        <div class="heading1">
+          <h1 class="text text-large">Sign In</h1>
+          <p class="text text-normal">New user? <span><Link to="/signup" class="text text-links">Create an account</Link></span>
+          </p>
+        </div>
+        <form name="signin" class="form">
+          <div class="input-control">
+            <label for="email" class="input-label" hidden>Email Address</label>
+            <input type="email" name="email" id="email" class="input-field" placeholder="Email Address"/>
+          </div>
+          <div class="input-control">
+            <label for="password" class="input-label" hidden>Password</label>
+            <input type="password" name="password" id="password" class="input-field" placeholder="Password"/>
+          </div>
+          <div class="input-control">
+            <Link href="#" class="text text-links">Forgot Password</Link>
+            <input type="submit" name="submit" class="input-submit" value="Sign In" disabled/>
+          </div>
+        </form>
+        
+      </section>
+    </div>
+  </main></div>
   );
 }
 
