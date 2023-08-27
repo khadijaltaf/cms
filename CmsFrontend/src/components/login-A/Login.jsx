0884ba21ce2dbfa27452139555af814b6a922c71
Alishba Ghori <ghorighori300@gmail.com>
2023-08-27 13:15:34 -0700

    Login-page

--- CmsFrontend/src/components/login-A/Login.jsx
+++ CmsFrontend/src/components/login-A/Login.jsx
@@ -1,10 +1,76 @@
-import React from 'react'
-import { Link, Outlet } from 'react-router-dom';
+// import React from 'react'
+// import { Link, Outlet } from 'react-router-dom';
 
-const Login = () => {
+// const Login = () => {
+//   return (
+//     <div>Login</div>
+//   )
+// }
+
+// export default Login
+
+/////////////
+/////////////
+
+import React, { useState } from 'react';
+import './Login.css';
+
+function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const handleLogin = () => {
+    alert(`Email: ${email}\nPassword: ${password}`);
+  };
+
   return (
-    <div>Login</div>
-  )
+    <div className="login-container">
+      <h2>Login</h2>
+      <div className="input-container">
+        <input
+          type="email"
+          placeholder="Username/Email"
+          value={email}
+          onChange={handleEmailChange}
+          required
+        />
+      </div>
+      <div className="input-container">
+        <input
+          type={showPassword ? 'text' : 'password'}
+          placeholder="Password"
+          value={password}
+          onChange={handlePasswordChange}
+          required
+        />
+        <span className="eye-icon" onClick={togglePasswordVisibility}>
+          {showPassword ? '👁️' : '👁️‍🗨️'}
+        </span>
+      </div>
+      <button className="login-button" onClick={handleLogin}>
+        Login
+      </button>
+      <p className="signup-link">
+        Don't have an account? <a href="#">Sign up</a>
+      </p>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
+
+
+
